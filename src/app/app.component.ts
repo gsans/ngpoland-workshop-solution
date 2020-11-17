616ea2c2df1e0657bebaae439df7db3b17e4a67e
Gerard Sans <gsans@amazon.com>
2020-11-17 21:19:43 +0000

    Initial app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, OnDestroy } from '@angular/core';
+import { onAuthUIStateChange, AuthState } from '@aws-amplify/ui-components';
+import Auth from '@aws-amplify/auth';
+import { DataStore, Predicates } from "@aws-amplify/datastore";
+import { Chatty } from "../models";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'amplify-datastore';
+export class AppComponent implements OnInit, OnDestroy {
+  user;
+  unregister;
+  moment = moment;
+  messages: Array<Chatty>;
+  public createForm: FormGroup;
+  subscription;
+
+  loadMessages() {
+    DataStore.query<Chatty>(Chatty, Predicates.ALL)
+      .then(messages => {
+        this.messages = [...messages].sort((a, b) => -a.createdAt.localeCompare(b.createdAt));
+      })
+  }
+
+  constructor(private fb: FormBuilder, private ref: ChangeDetectorRef) {
+    Auth.currentAuthenticatedUser().then(console.log);
+    this.createForm = this.fb.group({
+      'message': ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.unregister = onAuthUIStateChange((state, user) => {
+      if (state === AuthState.SignedIn) {
+        this.user = user;
+        this.ref.detectChanges();
+      }
+    })
+    this.loadMessages();
+    this.subscription = DataStore.observe<Chatty>(Chatty).subscribe(msg => {
+      console.log(msg.model, msg.opType, msg.element);
+      this.loadMessages();
+    });
+  }
+  ngOnDestroy() {
+    this.unregister();
+  }
+  public onCreate(message: any) {
+    if (message.message == "") return;
+
+    DataStore.save(new Chatty({
+      user: this.user.username,
+      message: message.message,
+      createdAt: new Date().toISOString()
+    })).then(() => {
+      console.log('message created!');
+      this.createForm.reset();
+      this.loadMessages();
+    })
+      .catch(e => {
+        console.log('error creating message...', e);
+      });
+  }
+  public async onDeleteAll() {
+    await DataStore.delete<Chatty>(Chatty, Predicates.ALL)
+      .then(() => this.loadMessages())
+      .catch(e => {
+        console.log('error deleting all messages...', e);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AmplifyUIAngularModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
